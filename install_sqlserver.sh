a767905a47b46f3b2702d97a513b2ef7d5abf29c
秦凯 <qinkai@sinosoft.com.cn>
2019-05-07 22:23:13 +0800

    correct script

--- install_sqlserver.sh
+++ install_sqlserver.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo 'start to pull sqlserver2017'
-sudo docker pull microsoft/mssql-server-linux:2017-latest
-echo 'finish pulling sqlserver2017'
-
-#声明变量，自定义控制变量名
+#before start ro run the script, you need to configure the PT_STORE2017_ABS_PATH and PT_STORE_JCGL_SK_ABS_PATH
 PT_STORE2017_ABS_PATH=/Users/qinkai/Desktop/PT_STORE2017_20190327.bak
 PT_STORE_JCGL_SK_ABS_PATH=/Users/qinkai/Desktop/PT_STORE_JCGL_SK-20190412.BAK
 PT_STORE2017_FILE=`basename $PT_STORE2017_ABS_PATH`
 PT_STORE_JCGL_SK_FILE=`basename $PT_STORE_JCGL_SK_ABS_PATH`
+
+echo 'start to pull sqlserver2017'
+sudo docker pull microsoft/mssql-server-linux:2017-latest
+echo 'finish pulling sqlserver2017'
 
 sudo docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=eMbMs1234!' -e 'MSSQL_COLLATION=Chinese_PRC_CI_AS' -p 1433:1433 --name sqlserver -d microsoft/mssql-server-linux:2017-latest
 
